8a152be29dc84a9c8ece279d533a9fc4a3db74fa
andrei <kkulmail@yandex.ru>
2023-09-25 19:07:17 +0200

    Задаем массив с клавиатуры

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Write("Введите количество элементов массива\t");
+int elementsCount = int.Parse(Console.ReadLine()!);
+string[] array= new string[elementsCount];
+
+for(int i = 0; i<array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}:\t");
+    array[i]= (Console.ReadLine()!);
+}
